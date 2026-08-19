64550c6e6e8c2fdf0d9deed9b8b76975f6fe9c8b
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-19 23:14:35 +0300

    fix: resolve visual ingest test sources from tests directory

--- tests/visual_feature_track_ingest.cmake
+++ tests/visual_feature_track_ingest.cmake
@@ -1,11 +1,15 @@
-add_executable(test_visual_feature_track_manager test_visual_feature_track_manager.cpp)
+add_executable(test_visual_feature_track_manager
+    "${CMAKE_CURRENT_LIST_DIR}/test_visual_feature_track_manager.cpp"
+)
 target_link_libraries(test_visual_feature_track_manager PRIVATE
     drone_test_support
     Eigen3::Eigen
 )
 drone_register_gtest(test_visual_feature_track_manager "unit;navigation;visual-feature-tracking")
 
-add_executable(test_visual_feature_track_ingest test_visual_feature_track_ingest.cpp)
+add_executable(test_visual_feature_track_ingest
+    "${CMAKE_CURRENT_LIST_DIR}/test_visual_feature_track_ingest.cpp"
+)
 target_link_libraries(test_visual_feature_track_ingest PRIVATE
     drone_test_support
     ${DRONE_PHASE17_TEST_CORE}
@@ -13,7 +17,9 @@ target_link_libraries(test_visual_feature_track_ingest PRIVATE
 )
 drone_register_gtest(test_visual_feature_track_ingest "unit;navigation;shadow-only;visual-feature-tracking")
 
-add_executable(visual_feature_track_ingest_replay visual_feature_track_ingest_replay.cpp)
+add_executable(visual_feature_track_ingest_replay
+    "${CMAKE_CURRENT_LIST_DIR}/visual_feature_track_ingest_replay.cpp"
+)
 target_link_libraries(visual_feature_track_ingest_replay PRIVATE
     ${DRONE_PHASE17_TEST_CORE}
     Eigen3::Eigen
